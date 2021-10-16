2a05ddde6512bdd3590bf39a5895a73c07119fe0
Phenix_Jay <pengyj1984@126.com>
2021-10-17 00:54:39 +0800

    fix curl_helper

--- curl_helper.h
+++ curl_helper.h
@@ -3,7 +3,6 @@
 #include <curl/curl.h>
 #include "data.h"
 
-
 const std::string token = "62b11c372bbf05ffeda21dc10bd51bc2";
 const std::string dig = "http://47.104.220.230/dig";
 const std::string formula = "http://47.104.220.230/formula";
@@ -14,7 +13,8 @@ size_t receive_data(void *contents, size_t size, size_t nmemb, void *stream){
     return size * nmemb;
 }
 
-CURLcode PostDig(const DigData &data, std::string &response){
+inline int PostDig(const std::string &locationId){
+    DigData data(locationId, token);
     std::stringstream ss;
     ajson::save_to(ss, data);
     CURLcode ret;
@@ -25,13 +25,18 @@ CURLcode PostDig(const DigData &data, std::string &response){
         return CURLE_FAILED_INIT;
     }
 
+    std::string response;
     ret = curl_easy_setopt(pCURL, CURLOPT_URL, dig.c_str());
     ret = curl_easy_setopt(pCURL, CURLOPT_POST, 1L);
 
     headers = curl_slist_append(headers, "application/json");
     ret = curl_easy_setopt(pCURL, CURLOPT_HTTPHEADER, headers);
 
-    ret = curl_easy_setopt(pCURL, CURLOPT_TIMEOUT, 300);
+    ret = curl_easy_setopt(pCURL, CURLOPT_POSTFIELDS, ss.str().c_str());
+
+    ret = curl_easy_setopt(pCURL, CURLOPT_TIMEOUT, 1);
+
+    ret = curl_easy_setopt(pCURL, CURLOPT_CONNECTTIMEOUT, 3);
 
     ret = curl_easy_setopt(pCURL, CURLOPT_WRITEFUNCTION, receive_data);
 
@@ -39,5 +44,11 @@ CURLcode PostDig(const DigData &data, std::string &response){
 
     ret = curl_easy_perform(pCURL);
     curl_easy_cleanup(pCURL);
-    return ret;
+
+    if (ret == CURLE_OK){
+        std::cout << response << std::endl;
+        return 0;
+    }
+
+    return 1;
 }
--- main.cpp
+++ main.cpp
@@ -18,11 +18,14 @@ MemPool *memPool;
 
 int totalCount = 0;
 int totalLines = 0;
+std::vector<RealData*> *realDatas;
 
 void HandleSourceData(MemChunk *chunk){
     // 一次处理过程能得到的有效数据应该不会
     std::vector<RealData*> *datas = new std::vector<RealData*>();
     int size = chunk->size;
+    int passCount = 0;
+    std::string response;
     for (int i = 0; i < size; ++i){
         auto temp = chunk->datas[i];
         SourceData src;
@@ -34,12 +37,18 @@ void HandleSourceData(MemChunk *chunk){
             if (locationNumber + (-1024) == magicNumber){
                 auto&& data = new RealData(src.locationid, magicNumber);
                 datas->push_back(data);
+                if (PostDig(src.locationid) == 0){
+                    passCount++;
+                }
                 continue;
             }
 
             if ((locationNumber & 1023) == magicNumber){
                 auto&& data = new RealData(src.locationid, magicNumber);
                 datas->push_back(data);
+                if (PostDig(src.locationid) == 0){
+                    passCount++;
+                }
                 continue;
             }
         }
@@ -47,12 +56,18 @@ void HandleSourceData(MemChunk *chunk){
             if (locationNumber + 1024 == magicNumber){
                 auto&& data = new RealData(src.locationid, magicNumber);
                 datas->push_back(data);
+                if (PostDig(src.locationid) == 0){
+                    passCount++;
+                }
                 continue;
             }
 
             if ((locationNumber << 10) == magicNumber){
                 auto&& data = new RealData(src.locationid, magicNumber);
                 datas->push_back(data);
+                if (PostDig(src.locationid) == 0){
+                    passCount++;
+                }
                 continue;
             }
         }
@@ -60,18 +75,19 @@ void HandleSourceData(MemChunk *chunk){
 
     {
         std::scoped_lock<std::mutex> g(settleMutex);
-        totalCount += datas->size();
+        totalCount += passCount;
+        if (datas->size() > 0)
+            realDatas->insert(realDatas->end(), datas->begin(), datas->end() - 1);
     }
     ///////
 //    totalCount += datas->size();
 //    if (datas->size() > 0){
 //        std::cout << "find " << totalCount << " datas in " << totalLines << " lines."<< std::endl;
 //    }
-    for (int i = 0; i < datas->size(); ++i){
-        delete (*datas)[i];
-    }
+//    for (int i = 0; i < datas->size(); ++i){
+//        delete (*datas)[i];
+//    }
     delete datas;
-
     memPool->Free(chunk);
 }
 
@@ -85,6 +101,8 @@ int main(int argc, char const *argv[]){
 
     // 初始化内存池
     memPool = new MemPool();
+
+    realDatas = new std::vector<RealData*>();
 
     // 从四个起点开始沿不同方向遍历所有文件
     int startPoint0 = 31, startPoint1 = 32, startPoint2 = 95, startPoint3 = 96;
@@ -108,49 +126,46 @@ int main(int argc, char const *argv[]){
 //    }
 
     // test 8 files
-    for (int i = 0; i < 8; ++i){
-        file = "./Treasure_" + std::to_string(i) + ".data";
-        std::ifstream ifstream(file);
-        if (!ifstream.is_open()){
-            std::cout << "file open failed." << std::endl;
-            return -1;
-        }
-        std::string temp;
-        auto&& chunk = memPool->Alloc();
-        while (getline(ifstream, temp)){
-            auto&& data = chunk->datas[chunk->size];
-            data.size = temp.size();
-            memcpy(data.buffer, temp.c_str(), data.size);
-            chunk->size++;
-            ++totalLines;
-            if (chunk->size >= MAXCHUNKSIZE){
-                //HandleSourceData(lines);
-                pool->Add([chunk](){
-                    HandleSourceData(chunk);
-                });
-                chunk = memPool->Alloc();
-            }
-        }
-        if (chunk->size > 0){
-            //HandleSourceData(lines);
-            pool->Add([chunk](){
-                HandleSourceData(chunk);
-            });
-        }
-        ifstream.close();
-    }
+//    for (int i = 0; i < 8; ++i){
+//        file = "./Treasure_" + std::to_string(i) + ".data";
+//        std::ifstream ifstream(file);
+//        if (!ifstream.is_open()){
+//            std::cout << "file open failed." << std::endl;
+//            return -1;
+//        }
+//        std::string temp;
+//        auto&& chunk = memPool->Alloc();
+//        while (getline(ifstream, temp)){
+//            auto&& data = chunk->datas[chunk->size];
+//            data.size = temp.size();
+//            memcpy(data.buffer, temp.c_str(), data.size);
+//            chunk->size++;
+//            ++totalLines;
+//            if (chunk->size >= MAXCHUNKSIZE){
+//                //HandleSourceData(lines);
+//                pool->Add([chunk](){
+//                    HandleSourceData(chunk);
+//                });
+//                chunk = memPool->Alloc();
+//            }
+//        }
+//        if (chunk->size > 0){
+//            //HandleSourceData(lines);
+//            pool->Add([chunk](){
+//                HandleSourceData(chunk);
+//            });
+//        }
+//        ifstream.close();
+//        usleep(500);           // 休眠500微秒(0.5毫秒)
+//    }
 
     while (pool->JobsCount() > 0){
         usleep(2000);           // 休眠2000微秒(2毫秒)
     }
 
-//    std::stringstream ss;
-//    DigData data("fdsafadsfasdf", token);
 ////    ajson::save_to(ss, data);
 ////    std::cout << "json: " << ss.str() << std::endl;
-//    std::string response;
-//    auto ret = PostDig(data, response);
-//    std::cout << "response: " << response << std::endl;
+    auto ret = PostDig("01mq1k08v4qxl8f8yy9677oup67klu3ydzhr7xixqdbmcaz3lfwdkk04dl58fk8d");
 
     std::cout << "find " << totalCount << " datas in " << totalLines << " lines." << std::endl;
 
@@ -160,4 +175,8 @@ int main(int argc, char const *argv[]){
     std::cout << "total count in pool = " << memPool->TotalCount() << std::endl;
 
     delete memPool;
+    for (int i = 0; i < realDatas->size(); ++i){
+        delete (*realDatas)[i];
+    }
+    delete realDatas;
 }
